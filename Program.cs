5696587eadeb43933998111ee569ac4f45b877ef
Mikayel Badalyan <mikayel.b.a@gmail.com>
2024-12-23 03:15:23 +0400

    Reset to the previous commit

--- Program.cs
+++ Program.cs
@@ -47,10 +47,12 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
@@ -58,4 +60,3 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
-
